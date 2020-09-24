bbf81f9577d89bd29c01a250fdab6af3f68221ca
Сергей <sv.knyazev13@gmail.com>
2020-09-24 11:57:57 +0300

    practice 2

---
+++ practice2/src/Ball_Task.java
@@ -0,0 +1,15 @@
+public class Ball_Task
+{
+    public static void main(String[] args)
+    {
+        Ball b1 = new Ball ("black ","basketball",5);
+        Ball b2 = new Ball ("white ","football",4);
+        Ball b3 = new Ball ("brown ","volleyball",7);
+        b3.setRadius(1);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println();
+
+    }
+}
